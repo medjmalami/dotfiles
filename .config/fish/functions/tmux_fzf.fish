0ba925a5c4978d43d7fb66848a4d576929b2cbbc
amine <jmalmohamedamine1@gmail.com>
2026-07-28 18:44:39 +0100

    change to amine

--- .config/fish/functions/tmux_fzf.fish
+++ .config/fish/functions/tmux_fzf.fish
@@ -3,7 +3,7 @@ function tmux_fzf
     set prev (pwd)
     zi
     set name (basename (pwd))
-    if test "$name" != amine
+    if test "$name" != $USER
         if tmux has-session -t $name
             tmux attach -t $name
         else
